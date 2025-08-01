70de843359cd9bef56951ac877a582c157e7d023
gordobil <noahgordobil@gmail.com>
2025-08-01 18:24:48 +0200

    everything_working

--- src/game/minimap.c
+++ src/game/minimap.c
@@ -6,7 +6,7 @@
 /*   By: ngordobi <ngordobi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 11:32:29 by ngordobi          #+#    #+#             */
-/*   Updated: 2025/07/29 18:47:03 by ngordobi         ###   ########.fr       */
+/*   Updated: 2025/08/01 18:14:10 by ngordobi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,16 @@
 
 void	draw_minimap_pointer(t_cub3d *cub3d, int x, int y)
 {
-	x += 4;
-	y += 4;
+
 	my_pixel_put(cub3d->img, x, y, BLUE);
-	if (cub3d->player->ang > 335 || cub3d->player->ang < 24)
-		my_pixel_put(cub3d->img, x + 1, y, RED);
-	else if (cub3d->player->ang > 23 && cub3d->player->ang < 67)
-		my_pixel_put(cub3d->img, x + 1, y + 1, RED);
-	else if (cub3d->player->ang > 66 && cub3d->player->ang < 114)
-		my_pixel_put(cub3d->img, x, y + 1, RED);
-	else if (cub3d->player->ang > 113 && cub3d->player->ang < 157)
-		my_pixel_put(cub3d->img, x - 1, y + 1, RED);
-	else if (cub3d->player->ang > 156 && cub3d->player->ang < 203)
-		my_pixel_put(cub3d->img, x - 1, y, RED);
-	else if (cub3d->player->ang > 202 && cub3d->player->ang < 246)
-		my_pixel_put(cub3d->img, x - 1, y - 1, RED);
-	else if (cub3d->player->ang > 245 && cub3d->player->ang < 293)
-		my_pixel_put(cub3d->img, x, y - 1, RED);
-	else if (cub3d->player->ang > 292 && cub3d->player->ang < 336)
-		my_pixel_put(cub3d->img, x + 1, y - 1, RED);
+	my_pixel_put(cub3d->img, x + 1, y, RED);
+	my_pixel_put(cub3d->img, x + 1, y + 1, RED);
+	my_pixel_put(cub3d->img, x, y + 1, RED);
+	my_pixel_put(cub3d->img, x - 1, y + 1, RED);
+	my_pixel_put(cub3d->img, x - 1, y, RED);
+	my_pixel_put(cub3d->img, x - 1, y - 1, RED);
+	my_pixel_put(cub3d->img, x, y - 1, RED);
+	my_pixel_put(cub3d->img, x + 1, y - 1, RED);
 }
 
 void	draw_minimap(t_cub3d *cub3d)
